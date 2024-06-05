92e3a3fe160f624551d2d7dbdf8d4db43f6460ac
auumeiss <159965679+auumeiss@users.noreply.github.com>
2024-06-05 14:00:47 +0300

    Add files via upload

---
+++ IGI/LR3/lab 1/lab3.py
@@ -0,0 +1,34 @@
+# Подсчет слов со строчной буквы
+
+# Задание 3. Не использовать регулярные выражения. В соответствии с заданием своего
+# варианта составить программу для анализа текста, вводимого с клавиатуры.
+
+# В строке, вводимой с клавиатуры, подсчитать количество слов,
+# начинающихся со строчной буквы
+def count_lowercase_start_words(text):
+    # Разделение текста на слова
+    words = text.split()
+
+    # Подсчет количества слов, начинающихся со строчной буквы
+    count = 0
+    for word in words:
+        if word[0].islower():
+            count += 1
+    return count
+
+
+def lowercase_start_words_counter(func):
+    def wrapper():
+        # Ввод текста с клавиатуры
+        text = input("Введите текст: ")
+        # Подсчет слов, начинающихся со строчной буквы
+        lowercase_start_words_count = func(text)
+        # Вывод результата
+        print(f"Количество слов, начинающихся со строчной буквы: {lowercase_start_words_count}")
+
+    return wrapper
+
+
+@lowercase_start_words_counter
+def lab3(text):
+    return count_lowercase_start_words(text)
